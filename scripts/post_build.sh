2e3c8a6f59796774a29dad0f6ad834cda49f54c6
Javier Nualart Lee <nualartlee@gmail.com>
2019-02-04 14:48:28 -0800

    Corrected docker compose
    On branch initial
    Your branch is up to date with 'origin/initial'.
    
    Changes committed:
    
    	modified:   docker-compose.yml
    	modified:   scripts/post_build.sh

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -8,12 +8,6 @@ cd "${0%/*}/.."
 # Import common functions
 source deploy-scripts/common.sh
 
-# Run Scrapy setup
-echo
-echo "Running scrapyd_setup.sh"
-docker-compose exec scrapyd ./scrapyd_setup.sh
-check_errs $? "Failed setting up scrapyd"
-
 # Run Django setup
 echo
 echo "Running django_setup.py"
