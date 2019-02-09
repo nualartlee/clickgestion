6344acc0c3bcc347aa1beeb814ae8d96e2cce937
Javier Nualart Lee <nualartlee@gmail.com>
2019-02-09 11:34:48 -0800

    Cleaning up
    On branch initial
    Your branch is up to date with 'origin/initial'.
    
    Changes committed:
    
    	modified:   django_container/app/clickgestion/apartment_rentals/templates/comments/comment_detail.html
    	modified:   django_container/app/clickgestion/apartment_rentals/templates/comments/comment_list.html
    	modified:   django_container/app/clickgestion/core/templates/core/index.html
    	modified:   django_container/app/clickgestion/core/templates/core/login.html
    	modified:   django_container/app/clickgestion/core/urls.py
    	modified:   django_container/app/clickgestion/transactions/templates/transactions/transaction.html

--- django_container/app/clickgestion/core/urls.py
+++ django_container/app/clickgestion/core/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/', auth_views.login, {'template_name': 'main/login.html'}, name='login'),
+    url(r'^login/', auth_views.login, {'template_name': 'core/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
