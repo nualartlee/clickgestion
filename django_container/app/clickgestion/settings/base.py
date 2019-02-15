8e02c3eb8c4dc263d0f1fd2734a2519cae32aa46
Javier Nualart Lee <nualartlee@gmail.com>
2019-02-15 15:25:09 -0800

    Overriding admin base template
    On branch initial
    Your branch is up to date with 'origin/initial'.
    
    Changes committed:
    
    	new file:   django_container/app/clickgestion/core/templates/admin/base.html
    	modified:   django_container/app/clickgestion/settings/base.py

--- django_container/app/clickgestion/settings/base.py
+++ django_container/app/clickgestion/settings/base.py
@@ -70,7 +70,9 @@ ROOT_URLCONF = 'clickgestion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'core', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
