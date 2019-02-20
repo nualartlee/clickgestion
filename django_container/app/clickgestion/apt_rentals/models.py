c12ec8b4a66d537429d9ac6a38519abf2066b391
Javier Nualart Lee <nualartlee@gmail.com>
2019-02-20 15:00:04 -0800

    Developing deposit model
    On branch initial
    Your branch is up to date with 'origin/initial'.
    
    Changes committed:
    
    	modified:   django_container/app/clickgestion/apt_rentals/models.py

--- django_container/app/clickgestion/apt_rentals/models.py
+++ django_container/app/clickgestion/apt_rentals/models.py
@@ -165,4 +165,101 @@ class AptRentalSettings(ConceptSettings):
         verbose_name_plural = gettext_lazy('Apartment Rental Settings')
 
 
+class AptRentalDepositSettings(ConceptSettings):
+    """
+    Apartment Rental Deposit Settings
+    """
+    # Maximum deposit amount
+    max = models.FloatField(verbose_name=gettext_lazy('Maximum Amount'), default=300)
+    # Minimum deposit amount
+    min = models.FloatField(verbose_name=gettext_lazy('Minimum Amount'), default=50)
+    # Amount per adult/night
+    per_adult = models.FloatField(verbose_name=gettext_lazy('Amount Per Adult/Night'), default=10)
+    # Amount per child/night
+    per_child = models.FloatField(verbose_name=gettext_lazy('Amount Per Child/Night'), default=5)
+
+
+    class Meta:
+        verbose_name = gettext_lazy('Apartment Rental Deposit Settings')
+        verbose_name_plural = gettext_lazy('Apartment Rental Deposit Settings')
+
+
+class AptRentalDepositCharge(ConceptData):
+    """
+    Transaction Concept
+    Apartment rental deposit charge
+    """
+    # Number of adults
+    adults = models.SmallIntegerField(verbose_name=gettext_lazy('Adults'), default=2)
+    # Number of children
+    children = models.SmallIntegerField(verbose_name=gettext_lazy('Children'), default=0)
+    # Number of nights
+    nights = models.SmallIntegerField(verbose_name=gettext_lazy('Nights'), default=7)
+    # Amount when saved
+    amount = models.FloatField(verbose_name=gettext_lazy('Amount'), default=0)
+
+    class Meta:
+        verbose_name = gettext_lazy('Apartment Rental Deposit Charge')
+        verbose_name_plural = gettext_lazy('Apartment Rental Deposit Charges')
+
+    def __str__(self):
+        return self.code
+
+    @property
+    def code_initials(self):
+        """
+        :return: ARD for apartment rental deposit
+        """
+        return 'ARD'
+
+    @property
+    def description_short(self):
+        desc = gettext_lazy('Refundable Deposit') + '; '
+        desc += gettext_lazy('Adults') + ': {}, '.format(self.adults)
+        desc += gettext_lazy('Children') + ': {}, '.format(self.children)
+        desc += gettext_lazy('Nights') + ': {}'.format(self.nights)
+        return desc
+
+    @property
+    def description_long(self):
+        return self.description_short
+
+    @property
+    def price(self):
+        """
+        :return: Amount to deposit
+        """
+        total =  (self.adults * self.settings.per_adult + self.children * self.settings.per_child) * self.nights
+        if total > self.settings.max:
+            return self.settings.max
+        if total < self.settings.min:
+            return self.settings.min
+        return total
+
+    def save(self, *args, **kwargs):
+        # Set or create the value
+        try:
+            value = self.value
+            value.amount = self.price
+            value.save()
+        except ConceptValue.DoesNotExist:
+            value = ConceptValue.objects.create(
+                amount=self.price,
+            )
+            self.value = value
+        # save
+        super().save(*args, **kwargs)
+
+    @property
+    def settings(self):
+        return AptRentalDepositSettings.objects.get_or_create()[0]
+
+    @property
+    def type(self):
+        return gettext('Apartment Rental Deposit')
+
+    @property
+    def url(self):
+        return '/apt-rental-deposits/{}'.format(self.id)
+
 
