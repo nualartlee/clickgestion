ba93da8b1345257424d7c18a2185412ff1656ff8
Javier Nualart Lee <nualartlee@gmail.com>
2019-02-28 15:51:44 -0800

    Developing transaction forms
    On branch initial
    Your branch is up to date with 'origin/initial'.
    
    Changes committed:
    
    	modified:   django_container/app/clickgestion/transactions/filters.py
    	modified:   django_container/app/clickgestion/transactions/templates/transactions/transaction_list.html

--- django_container/app/clickgestion/transactions/filters.py
+++ django_container/app/clickgestion/transactions/filters.py
@@ -8,36 +8,22 @@ from crispy_forms.layout import Layout, Field, Row, Column
 class TransactionFilter(django_filters.FilterSet):
 
     code = django_filters.CharFilter(lookup_expr='icontains')
-    code.field.widget.attrs['placeholder'] = gettext_lazy('Search by code')
+    code.field.label = gettext_lazy('Reference')
+    code.field.widget.attrs['placeholder'] = gettext_lazy('Search by reference number')
     code.field.widget.attrs['class'] = 'form-control'
     closed_date = django_filters.DateFromToRangeFilter()
+    closed_date.field.label = gettext_lazy('Date Range')
+    closed_date.field.widget.attrs['type'] = 'date'
+    closed_date.field.widget.attrs['class'] = 'dateinput form-control'
 
     class Meta:
         model = Transaction
         fields = ['code', 'closed_date', 'apt_number', 'client_first_name', 'client_last_name', 'employee',]
 
-    #def __init__(self, *args, **kwargs):
-    #    super().__init__(*args, **kwargs)
-    #    self.helper = FormHelper()
-    #    self.helper.form_tag = False
-    #    #self.helper.layout = Layout(
-    #    #    Row(
-    #    #        Column(
-    #    #            Field(
-    #    #                'checkin',
-    #    #                title=gettext_lazy("Arrival date"),
-    #    #                css_class='col-8',
-    #    #            ),
-    #    #            css_class='col-6',
-    #    #        ),
-    #    #        Column(
-    #    #            Field(
-    #    #                'checkout',
-    #    #                title=gettext_lazy("Departure date"),
-    #    #                css_class='col-8',
-    #    #            ),
-    #    #            css_class='col-6',
-    #    #        ),
-    #    #    ),
-    #    #)
+    @property
+    def form(self):
+        form = super().form
+        form.helper = FormHelper()
+        form.helper.form_tag = False
+        return form
 
