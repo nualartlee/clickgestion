10f21922f78b20d145b04b2971f94b95524ae70b
Javier Nualart Lee <nualartlee@gmail.com>
2019-03-19 17:01:12 -0700

    Catching up with coverage
    On branch initial
    Your branch is up to date with 'origin/initial'.
    
    Changes committed:
    
    	modified:   django_container/app/clickgestion/core/test_utilities.py

--- django_container/app/clickgestion/core/test_utilities.py
+++ django_container/app/clickgestion/core/test_utilities.py
@@ -3,6 +3,10 @@ from clickgestion.core import model_creation
 from clickgestion.core.utilities import custom_permission_required
 from clickgestion.core.test import CustomTestCase
 from unittest.mock import Mock
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 
 class UtilitiesTest(CustomTestCase):
@@ -12,16 +16,35 @@ class UtilitiesTest(CustomTestCase):
         required_permission = 'apt_rentals.add_aptrental'
         codename = required_permission.split('.')[1]
         permission = apps.get_model('auth.Permission').objects.get(codename=codename)
-        # No permission
-        testuser.user_permissions.remove(permission)
-        testuser.save()
-        self.client.force_login(testuser)
+
+        # Not logged in
         request = Mock()
-        request.user = Mock(return_value=testuser)
-        view = Mock()
-        response = custom_permission_required(permission)(view)(request)
-        self.assertEqual(type(response), Mock)
-        import pdb; pdb.set_trace()
+        request.configure_mock(user=AnonymousUser())
+        return_view = 'mockview'
+        view = Mock(return_value=return_view)
+        view.request = Mock(return_value=request)
+        response = custom_permission_required(required_permission)(view)(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/login/')
+
+        # No permission
+        request = Mock()
+        request.configure_mock(user=testuser)
+        return_view = 'mockview'
+        view = Mock(return_value=return_view)
+        self.assertRaises(PermissionDenied, custom_permission_required(required_permission)(view), request)
+
+        # Has permission
+        print('has permission')
+        testuser.user_permissions.add(permission)
+        testuser.save()
+        testuser = get_object_or_404(get_user_model(), id=testuser.id)
+        request = Mock()
+        request.configure_mock(user=testuser)
+        return_view = 'mockview'
+        view = Mock(return_value=return_view)
+        response = custom_permission_required(required_permission)(view)(request)
+        self.assertEqual(response, return_view)
 
 
 
