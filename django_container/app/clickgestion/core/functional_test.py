3c33dd811bae0977e8a190865e4ed4782faf7709
Javier Nualart Lee <nualartlee@gmail.com>
2019-04-25 15:25:19 -0700

    Developing selenium tests
    On branch selenium
    Your branch is up to date with 'origin/selenium'.
    
    Changes committed:
    
    	new file:   .gitignore
    	modified:   django_container/app/clickgestion/core/functional_test.py

--- django_container/app/clickgestion/core/functional_test.py
+++ django_container/app/clickgestion/core/functional_test.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
-from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
+from selenium.webdriver.support import expected_conditions as ec # available since 2.26.0
 from django.test import TestCase
 
 
@@ -11,6 +12,7 @@ class FunctionalTestCase(TestCase):
     site_url = 'http://nginx/'
     selenium_url = 'http://selenium:4444/wd/hub'
     _browser = None
+    _wait = None
 
     def setUp(self):
         test_name = self._testMethodName
@@ -30,6 +32,12 @@ class FunctionalTestCase(TestCase):
         self._browser = webdriver.Remote(self.selenium_url, capabilities)
         print('Browser ready')
         return self._browser
+
+    @property
+    def wait(self):
+        if not self._wait:
+            self._wait = WebDriverWait(self.browser, 5)
+        return self._wait
 
     @property
     def browser(self):
@@ -59,11 +67,28 @@ class FunctionalTestCase(TestCase):
         elem.send_keys(Keys.RETURN)
         elem = self.browser.find_element_by_id('submit-id-submit')
         elem.click()
-        print(self.browser.page_source)
-        import pdb;pdb.set_trace()
+        self.wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '.btn-outline-primary')))
+
+    def go_home(self):
+        logo = self.browser.find_element_by_css_selector('.img > img:nth-child(1)')
+        logo.click()
+        self.wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '.btn-outline-primary')))
+
+    def new_transaction(self):
+        # Navigate to index
+        self.go_home()
+        # Click on new transaction button
+        elem = self.browser.find_element_by_css_selector('.btn-outline-primary')
+        elem.click()
+        self.wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '#dropdownMenuLink')))
+        # Click on dropdown
+        elem = self.browser.find_element_by_css_selector('#dropdownMenuLink')
+        elem.click()
 
 
 if __name__ == "__main__":
     test_case = FunctionalTestCase()
     test_case.login('dani', 'dani')
+    test_case.go_home()
+    test_case.new_transaction()
 
